6178b81ce73418b296d301b4bb41bef713885bec
liakh10 <119670997+liakh10@users.noreply.github.com>
2026-09-08 23:18:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* ================= CONFIG: единственное место, где меняются данные ================= */
 window.SITE = {
-  CA:      "SOON",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
+  CA:      "0xECA47e235859aE86963d8690F2D6e157deCb602E",                       /* строго "SOON" до лаунча, потом 0x… (42 символа) */
   X_URL:   "https://x.com/clawedcat_rh",
   NAME:    "Clawed Cat",
   TICKER:  "$CLAWED",
